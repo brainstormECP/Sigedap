04412b44aafb7a3d6a6aba1f0f8dc3ef95bd9799
Elvis <elvis.crego@gmail.com>
2019-12-22 23:27:50 -0500

    Agregando proyecto web

---
+++ Sigedap/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Sigedap.Helpers;
+
+[assembly: OwinStartupAttribute(typeof(Sigedap.Startup))]
+namespace Sigedap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
