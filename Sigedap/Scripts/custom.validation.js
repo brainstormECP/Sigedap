b26b49e3fa5f6f42a8e6c17ee889d851a51771ae
Elvis <elvis.crego@gmail.com>
2019-12-22 23:29:48 -0500

    Agregando css y js

---
+++ Sigedap/Scripts/custom.validation.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../Content/js/jquery-1.10.2.min.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+
+
+jQuery.validator.addMethod("mayor", function (value, element, param) {
+    return Date.parse(value) >= Date.parse($(param).val());
+});
+
+jQuery.validator.unobtrusive.adapters.add("mayor", ["otro"], function(options) {
+    options.rules["mayor"] = "#" + options.params.otro;
+    options.messages["mayor"] = options.message;
+});
